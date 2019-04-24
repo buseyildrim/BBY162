ea05e74ccba8992e6747f6d4378838d40a2c06a9
buseyildrim <48107096+buseyildrim@users.noreply.github.com>
2019-04-24 14:32:58 +0300

    Add files via upload

---
+++ uygulama04.py
@@ -0,0 +1,24 @@
+#Buse Yıldırım
+#comment1
+
+metin = "Açık bilim, araştırma çıktılarına ve süreçlerine herkesin serbestçe erişmesini, bunların ortak kullanımını, dağıtımını ve üretimini kolaylaştıran bilim uygulamasıdır."
+print(metin[ :20])
+
+#comment2
+liste = ["Açık Bilim", "Açık Erişim", "Açık Lisans", "Açık Eğitim", "Açık veri", "Açık Kültür"]
+
+for i in liste:
+    print(i)
+
+#comment3
+sozluk = {"Elma" : "Ağaçta yetişen bir tür meyve" , "Salatalık" : "Fidan üzerinde büyüyen bir tür sebze" }
+kelime=[]
+
+sozluk=input("Öğrenmek istediğiniz kelimeyi giriniz!")
+    cevap = input("kelime:")
+    cevap = cevap.capitalize()
+    if cevap in sozluk:
+        print(sozluk[cevap])
+    else:
+        print("kelime yok")
+        
